265e864d487aca78023a69155f585cb390cbc17d
Ruth Chisom <ndukwe100805043@gmail.com>
2025-05-09 14:20:40 +0100

    switch provider to celo

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const { ethers } = require("ethers");
 const SplitzyAbi = require("./abi/Splitzy.json");
 
-const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545"); // or Alchemy/Celo provider
-const contractAddress = "YOUR_DEPLOYED_CONTRACT_ADDRESS";
+const provider = new ethers.JsonRpcProvider("https://alfajores-forno.celo.org");
+const contractAddress = "0xA9Eaf8E76966b60e9aB63C74a42605E84adF9EcE";
 
 const splitzyContract = new ethers.Contract(contractAddress, SplitzyAbi, provider);
 
